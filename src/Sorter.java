0ba1c2f707f27528812e652681faa50e9fee014f
OmarBameqadam <s202182990@kfupm.edu.sa>
2023-09-24 19:17:37 +0300

    Implementing selectionSort Method

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,4 +2,18 @@ public class Sorter{
     public static void main(String[] args) {
         System.out.println("O");
     }
+    public static void selectionSort(int arr[]){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minimumIndex = i;
+            for (int j =i+1; j< arr.length;j++) {
+                if (arr[j] < arr[minimumIndex]) {
+                    minimumIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minimumIndex];
+            arr[minimumIndex] = temp;
+        }
+    }
 }
